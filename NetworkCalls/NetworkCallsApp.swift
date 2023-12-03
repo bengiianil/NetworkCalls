988bccee61aa489b914c8bd4b712b62e6ea257a4
Bengi Anıl <banil2@ford.com.tr>
2023-12-03 14:54:33 +0300

    Initial Commit

---
+++ NetworkCalls/NetworkCallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkCallsApp.swift
+//  NetworkCalls
+//
+//  Created by Bengi Anıl on 3.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkCallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
